336f63dac399d8cbd16a7d3ef8ac1c3e0d9325e1
savosburn <savosburn18@tampabay.rr.com>
2021-06-04 22:28:01 -0400

    Initial commit

---
+++ src/main/java/ex18/App.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Savannah Osburn
+ */
+
+package ex18;
+
+import java.util.Scanner;
+
+public class App {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        App myApp = new App();
+        
+    }
+}
